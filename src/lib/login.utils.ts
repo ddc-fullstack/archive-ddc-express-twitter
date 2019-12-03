461bcce5c59727a807fa76c88062483291af1032
gkephart <gkephart@cnm.edu>
2019-12-03 15:45:50 -0700

    created needed utils to handle auth with Passport

---
+++ src/lib/login.utils.ts
@@ -0,0 +1,36 @@
+import {Profile} from "../interfaces/Profile";
+
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+interface HashedPassword {
+    hash: string,
+    salt: string,
+
+}
+
+
+
+export function setPassword(password: string) : HashedPassword {
+    const salt = crypto.randomBytes(16);
+    const hash = crypto.pbkdf2Sync(password, salt, 238722, 512, "sha512").toString("hex");
+    return {hash, salt}
+
+}
+
+export function validatePassword(password: string, profile: Profile, salt: string) : boolean {
+    const unprovenHash  = crypto.pbkdf2Sync(password, salt, 238722, 512, "sha512").toString("hex");
+    return unprovenHash === profile.profileHash;
+}
+
+export function generateJwt(profile : Profile) : any {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        profileEmail: profile.profileEmail,
+        ProfileId: profile.profileId,
+        exp: expirationDate.getTime() / 1000
+    }, 'secret');
+}
