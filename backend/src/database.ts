7d411d6f1027c5e660e46cf3875122a88e50f78c
GKephart <gkephart@cnm.edu>
2020-05-16 14:23:01 -0600

    development container is finished

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -4,13 +4,15 @@ require('dotenv').config();
 // Database connection. Can use config from .env file (based on example.env) or the defaults
 
 export async function connect (): Promise<Pool> {
+
   const connection = await createPool({
-    host: 'localhost',
+    host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     connectionLimit: 10,
     namedPlaceholders: true
   });
+
   return connection;
 }
