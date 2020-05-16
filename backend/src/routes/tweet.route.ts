792236621b826cac0b52dc3aa04c1495921fd9c6
GKephart <gkephart@cnm.edu>
2020-05-16 14:11:41 -0600

    got node containerized. I still cannot connect to the database.ts

--- backend/src/routes/tweet.route.ts
+++ backend/src/routes/tweet.route.ts
@@ -10,7 +10,7 @@ const router = Router();
 
 // Every new route is instantiated below. It will include the controller name and the type of action (get, post, delete, put, patch)
 router.route('/')
-  .get(isLoggedIn, getAllTweets)
+  .get( getAllTweets)
   .post(asyncValidator(checkSchema(tweetValidator)), insertTweet);
 
 export default router;
